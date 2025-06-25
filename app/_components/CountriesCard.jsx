a1f72d5c45a2e24d9012eeda1cdce0b65f3dc098
Shehzad Ashraf <stewardmax5331@gmail.com>
2025-06-25 14:38:59 +0500

    Country Image Defualt Change

--- app/_components/CountriesCard.jsx
+++ app/_components/CountriesCard.jsx
@@ -6,8 +6,13 @@ export const CountriesCard = ({ country }) => {
     <Link href={`/country/${encodeURIComponent(country.name.common)}`}>
     <div className="p-4 border-2 rounded-2xl  text-center ">
      <img
-  src={country.flags?.png}
+  src={country.flags?.png||'/download.png'} 
   alt={country.name?.common}
+  onError={(e) =>{ 
+    e.target.onerror= null;
+    e.target.src ='/download.png';
+
+  }}
   className="w-152 h-[150px] object-contain mb-6 rounded-lg transition-all duration-150 cursor-pointer
    hover:scale-105 shadow-lg"
 />
