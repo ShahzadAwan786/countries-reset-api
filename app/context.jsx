749e0814f903747a0acb0529c098b8f43e5f8c67
Shehzad Ashraf <stewardmax5331@gmail.com>
2025-06-30 17:02:41 +0500

    LocalStorage

--- app/context.jsx
+++ app/context.jsx
@@ -4,7 +4,14 @@ import { createContext, useState, useContext, useEffect } from "react";
 const themeContext = createContext();
 
 export const ThemeProvider = ({ children }) => {
-  const [theme, settheme] = useState("light");
+  const [theme, setTheme] = useState("light");
+
+  useEffect(() => {
+    const savedTheme = localStorage.getItem("theme");
+    if (!!savedTheme) {
+      setTheme(savedTheme);
+    } 
+  }, [theme]);
 
   useEffect(() => {
     if (theme==="dark") {
@@ -16,11 +23,12 @@ export const ThemeProvider = ({ children }) => {
   }, [theme]);
 
   const toggleTheme = () => {
-    settheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+      setTheme((prevTheme) => (prevTheme === "light" ? "dark" : "light"));
+    localStorage.setItem("theme", theme === "light" ? "dark" : "light");  
   };
 
   return (
-    <themeContext.Provider value={{ theme, settheme, toggleTheme }}>
+    <themeContext.Provider value={{ theme, setTheme, toggleTheme }}>
       {children}
     </themeContext.Provider>
   );
